a7f6daeb1cb241cb77c8c015f437850d7ce91133
norman <norman.betancourtt.mairena@est.una.ac.cr>
2021-11-17 01:31:05 -0600

    depurando clasificador

--- main.py
+++ main.py
@@ -10,23 +10,26 @@ def lineReader(file):
     #O(n²)
     f = open(file, encoding="utf8")
     index =0
-    for linea in f:
+    for linea in f:# n -> cada linea
         lienaCruda = linea.split("\n")
         cantidad = len(lienaCruda)
-        lienaCruda = lienaCruda[:cantidad-1]
+        lienaCruda[0].strip("\n")
+
+
         index += 1
         if len(lienaCruda) > 0:
             print(f"Linea: {index}")
             print(lienaCruda[0])
-            print('\n')
+            palabras = lienaCruda[0].split(" ")
             if "(" in lienaCruda[0] and ")" in lienaCruda[0]:
-                print("es un nuevo scope")
+                #entra cuando se crea un nuevo scope
+                if  "if" in palabras or  "while" in palabras:
+                    print("es un condicional o ciclo")
+                else:
+                    print("es una funcion")
+            else:
+                print("code normal")
 
-        # if len(lienaCruda) > 0:
-        #     palabra = lienaCruda[0].split(" ")
-        #     for p in palabra:
-        #         if len(p) > 0:
-        #             print(p)
 
     f.close()
 
